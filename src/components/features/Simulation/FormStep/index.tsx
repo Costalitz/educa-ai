d0013e366de15c064e3a9ec32ab4daaff1b92ce5
Costalitz <PedroM_187@hotmail.com>
2026-09-09 20:59:12 -0300

    feat: implementa mascara de moeda no input

--- src/components/features/Simulation/FormStep/index.tsx
+++ src/components/features/Simulation/FormStep/index.tsx
@@ -3,6 +3,7 @@ import { type SyntheticEvent, useState } from 'react'
 
 import { Button } from '@/components/shared/Button'
 import { Input, type InputProps } from '@/components/shared/Input'
+import { formatCurrencyMask } from '@/utils/currency';
 
 export interface FormStepProps {
   id: string
@@ -56,7 +57,11 @@ export function FormStep({
         <Input
           {...inputProps}
           value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
+          onChange={(e) => {
+            const isCurrency = inputProps.prefix === 'R$'
+            const value = e.target.value
+            setInputValue(isCurrency ? formatCurrencyMask(value) : value)
+          }}
         />
         <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
           {!hideBackButton && (
